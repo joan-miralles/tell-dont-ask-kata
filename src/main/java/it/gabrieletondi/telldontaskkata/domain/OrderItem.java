6684b4231d5738475ef3d2eb06e84a86211556a8
Joan Miralles <joan.miralles@gmail.com>
2019-10-06 17:30:57 +0200

    Remove unused setters

--- src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/domain/OrderItem.java
@@ -8,9 +8,6 @@ public class OrderItem {
     private BigDecimal taxedAmount;
     private BigDecimal tax;
 
-    public OrderItem() {
-    }
-
     public OrderItem(Product product, int quantity) {
         this.product = product;
         this.quantity = quantity;
@@ -22,31 +19,16 @@ public class OrderItem {
         return product;
     }
 
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
     public int getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
     }
 
     public BigDecimal getTaxedAmount() {
         return taxedAmount;
     }
 
-    public void setTaxedAmount(BigDecimal taxedAmount) {
-        this.taxedAmount = taxedAmount;
-    }
-
     public BigDecimal getTax() {
         return tax;
     }
 
-    public void setTax(BigDecimal tax) {
-        this.tax = tax;
-    }
 }
