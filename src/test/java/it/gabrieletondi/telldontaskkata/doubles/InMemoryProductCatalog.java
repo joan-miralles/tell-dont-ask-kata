ef9fde4520604233935a66bf849074313d4c65a7
Joan Miralles <joan.miralles@gmail.com>
2019-10-06 17:08:37 +0200

    Move throw UnknownProductException to ProductCatalog

--- src/test/java/it/gabrieletondi/telldontaskkata/doubles/InMemoryProductCatalog.java
+++ src/test/java/it/gabrieletondi/telldontaskkata/doubles/InMemoryProductCatalog.java
@@ -2,6 +2,7 @@ package it.gabrieletondi.telldontaskkata.doubles;
 
 import it.gabrieletondi.telldontaskkata.domain.Product;
 import it.gabrieletondi.telldontaskkata.repository.ProductCatalog;
+import it.gabrieletondi.telldontaskkata.useCase.UnknownProductException;
 
 import java.util.List;
 
@@ -12,7 +13,11 @@ public class InMemoryProductCatalog implements ProductCatalog {
         this.products = products;
     }
 
+    @Override
     public Product getByName(final String name) {
-        return products.stream().filter(p -> p.getName().equals(name)).findFirst().orElse(null);
+        return products.stream()
+                .filter(p -> p.getName().equals(name))
+                .findFirst()
+                .orElseThrow(UnknownProductException::new);
     }
 }
